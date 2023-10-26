27b317e2ef35165054a31ecf0ecb834f68a4e4be
Coding_sup <juseop0915@gmail.com>
2023-10-26 14:51:30 +0000

    2023/10/26/2

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -9,6 +9,7 @@ public class HelloSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 
+		회원레포지토리 테스트 케이스 작성 부터 보면 된다잉 그리고 MemoryMemeberRepository 클래스에 오버라이딩한 메서드 중에 fingByName이거 람다식 이용해서 구현했으니깐 람다식부터 공부하도록.
 	}
 
 }
