eae9e8d9b264776cdbf8a4ab01175a27a2a89cb3
Coding_sup <juseop0915@gmail.com>
2023-10-31 08:25:14 +0000

    2023/10/31/5

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -8,8 +8,7 @@ public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
-
-		웹 mvc개발 그 파트부터 보면 된다잉.
+		
 	}
 
 }
